289bc0ff4d7b9bbf424b11025ea4a321eddbc9cb
Mike Aceves <acevesm77@gmail.com>
2021-02-25 17:11:52 -0600

    first commit, pwa created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import {
+  Container,
+  Row,
+  Card,
+  CardBody,
+  CardText,
+  Col
+} from 'reactstrap';
+
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState("Otra cosa")
+  const [bitcoinValues, setBitcoinValues] = useState({})
+
+  const changeHandler = event => {
+    let value = event.target.value
+    setTitle(value)
+  }
+
+  useEffect(() => {
+    fetch("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR,AUD,CNY,KRW")
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        setBitcoinValues(json)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col className="xs-12">
+            <h1 className="py-3">Costo del bitcoin en diferentes monedas</h1>
+            <Row>
+            {
+              Object.keys(bitcoinValues).map(key => {
+                return (
+                  <Col xs="6">
+                    <Card className="mb-3 bg-dark text-white shadow border rounded">
+                    <CardBody>
+                      <CardText>
+                        <p>BTC - { key }</p>
+                        <p><b>{ bitcoinValues[key] }</b></p>
+                      </CardText>
+                    </CardBody>
+                  </Card>
+                  </Col>
+                )
+              })
+            }
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
